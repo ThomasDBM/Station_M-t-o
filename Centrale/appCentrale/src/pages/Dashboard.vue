5ef5f7571fd01a327e92e524d91d7e65d795fca5
thomasDBM <thomasdebeaumont63@gmail.com>
2022-03-07 19:03:00 +0100

    finish the sonde dashboard, start the dash board

--- Centrale/appCentrale/src/pages/Dashboard.vue
+++ Centrale/appCentrale/src/pages/Dashboard.vue
@@ -1,199 +1,60 @@
 <template>
   <div class="content">
     <div class="md-layout">
+      <!--<div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33 ChooseStation"
+      >
+        <h4>Choose a Station :  </h4>
+        <li v-for="(station,index) in ChoosenStations" :key="index">
+          <md-checkbox v-model="string" @change="onChange" v-bind:value="station">{{station}}</md-checkbox>
+        </li>
+      </div>-->
+
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+        v-if="this.renderComponent"
       >
         <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
+          :chart-data="TemperatureChart.data"
+          :chart-options="TemperatureChart.options"
           :chart-type="'Line'"
           data-background-color="blue"
         >
           <template slot="content">
-            <h4 class="title">Daily Sales</h4>
-            <p class="category">
-              <span class="text-success"
-                ><i class="fas fa-long-arrow-alt-up"></i> 55%
-              </span>
-              increase in today sales.
-            </p>
+            <h4 class="title">Temperature chart</h4>
+
+            <label for="start">Start date:</label>
+            <input @change="LoadDatas" type="date" id="start" name="trip-start"
+                  v-model="TemperatureChart.dates.dateDebut"
+                  value="2022-03-04"
+                  min="2022-01-01" max="2022-12-31">
+
+            <label for="start">Stop date:</label>
+            <input @change="LoadDatas" type="date" id="stop" name="trip-stop"
+                  v-model="TemperatureChart.dates.datefin"
+                  value="2022-03-05"
+                  min="2022-01-01" max="2022-12-31">
+
+            <div class="md-list-item-content ">
+                <drop-down class="ChooseStation">
+                  <md-button
+                    slot="title"
+                    class="md-button md-just-icon md-simple"
+                    data-toggle="dropdown"
+                  >
+                    <md-icon>settings_input_antenna</md-icon>
+                  </md-button>
+                  <ul class="dropdown-menu">
+                    <li v-for="(station,index) in ChoosenStations" :key="index">
+                      <md-checkbox v-model="stations" @change="LoadDatas" v-bind:value="station">{{station}}</md-checkbox>
+                    </li>
+                  </ul>
+                </drop-down>
+              </div>
           </template>
 
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              updated 4 minutes ago
-            </div>
-          </template>
+          
         </chart-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="emailsSubscriptionChart.data"
-          :chart-options="emailsSubscriptionChart.options"
-          :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
-          :chart-type="'Bar'"
-          data-background-color="red"
-        >
-          <template slot="content">
-            <h4 class="title">Email Subscription</h4>
-            <p class="category">Last Campaign Performance</p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              updated 10 days ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="dataCompletedTasksChart.data"
-          :chart-options="dataCompletedTasksChart.options"
-          :chart-type="'Line'"
-          data-background-color="green"
-        >
-          <template slot="content">
-            <h4 class="title">Completed Tasks</h4>
-            <p class="category">Last Campaign Performance</p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              campaign sent 26 minutes ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="green">
-          <template slot="header">
-            <md-icon>store</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Revenue</p>
-            <h3 class="title">$34,245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>date_range</md-icon>
-              Last 24 Hours
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="orange">
-          <template slot="header">
-            <md-icon>content_copy</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Used Space</p>
-            <h3 class="title">
-              49/50
-              <small>GB</small>
-            </h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon class="text-danger">warning</md-icon>
-              <a href="#pablo">Get More Space...</a>
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="red">
-          <template slot="header">
-            <md-icon>info_outline</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Fixed Issues</p>
-            <h3 class="title">75</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>local_offer</md-icon>
-              Tracked from Github
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="blue">
-          <template slot="header">
-            <i class="fab fa-twitter"></i>
-          </template>
-
-          <template slot="content">
-            <p class="category">Folowers</p>
-            <h3 class="title">+245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>update</md-icon>
-              Just Updated
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-      >
-        <md-card>
-          <md-card-header data-background-color="orange">
-            <h4 class="title">Employees Stats</h4>
-            <p class="category">New employees on 15th September, 2016</p>
-          </md-card-header>
-          <md-card-content>
-            <ordered-table table-header-color="orange"></ordered-table>
-          </md-card-content>
-        </md-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-      >
-        <nav-tabs-card>
-          <template slot="content">
-            <span class="md-nav-tabs-title">Tasks:</span>
-            <md-tabs class="md-success" md-alignment="left">
-              <md-tab id="tab-home" md-label="Bugs" md-icon="bug_report">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-pages" md-label="Website" md-icon="code">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-posts" md-label="server" md-icon="cloud">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-            </md-tabs>
-          </template>
-        </nav-tabs-card>
       </div>
     </div>
   </div>
@@ -201,34 +62,40 @@
 
 <script>
 import {
-  StatsCard,
+  //StatsCard,
   ChartCard,
-  NavTabsCard,
-  NavTabsTable,
-  OrderedTable,
+  //NavTabsCard,
+  //NavTabsTable,
+  //OrderedTable,
 } from "@/components";
 
 export default {
   components: {
-    StatsCard,
+    //StatsCard,
     ChartCard,
-    NavTabsCard,
-    NavTabsTable,
-    OrderedTable,
+    //NavTabsCard,
+    //NavTabsTable,
+    //OrderedTable,
   },
   data() {
     return {
-      dailySalesChart: {
+      stations:[],
+      renderComponent:true,
+      TemperatureChart: {
+        dates:{
+          dateDebut: 0,
+          datefin: 0,
+        },
         data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]],
+          labels: [],
+          series: [],
         },
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
             tension: 0,
           }),
           low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          high: 0, 
           chartPadding: {
             top: 0,
             right: 0,
@@ -236,75 +103,69 @@ export default {
             left: 0,
           },
         },
-      },
-      dataCompletedTasksChart: {
-        data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]],
-        },
-
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0,
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          },
-        },
-      },
-      emailsSubscriptionChart: {
-        data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De",
-          ],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
-        },
-        options: {
-          axisX: {
-            showGrid: false,
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
       },
     };
   },
+    computed : {
+    ChoosenStations(){
+      return this.$store.state.Stations;
+    }
+  },
+  methods : {
+    LoadDatas(){
+      console.log("reload datas:")
+
+      let max = 0;
+      let min = 0;
+
+      console.log("debut :"+this.TemperatureChart.dates.dateDebut)
+      console.log("fin :"+this.TemperatureChart.dates.datefin)
+
+      this.TemperatureChart.data.labels = [];
+      this.TemperatureChart.data.series = [];
+
+      this.stations.forEach(station => {
+        console.log(station)
+        fetch("http://"+station+":8080/data/temperature/"+this.TemperatureChart.dates.dateDebut+","+this.TemperatureChart.dates.datefin)
+        .then(response => response.json())
+        .then(result => {
+
+          let localmax = Math.max(...result.temperature.value);
+          let localmin = Math.min(...result.temperature.value);
+
+          console.log("max : "+localmax);
+          console.log("min : "+ localmin);
+
+          if (localmax > max){
+            max = localmax;
+          }
+          if (localmin < min){
+            min = localmin;
+          }
+
+          this.TemperatureChart.data.labels = result.temperature.date;
+          this.TemperatureChart.data.series.push(result.temperature.value);
+          this.TemperatureChart.options.low = min-10;
+          this.TemperatureChart.options.high = max+10;
+
+          this.renderComponent = false;
+          this.$nextTick(() => {
+            // Add the component back in
+            this.renderComponent = true;
+          })
+        })
+
+      })
+    }
+  }
 };
 </script>
+<style scoped>
+
+  .ChooseStation{
+    display : flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
--- Centrale/appCentrale/src/pages/DashboardSonde.vue
+++ Centrale/appCentrale/src/pages/DashboardSonde.vue
@@ -1,59 +1,18 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33 ChooseStation"
-      >
-        <h4>Choose a Station :  </h4>
-        <li v-for="(station,index) in ChoosenStations" :key="index">
-          <md-checkbox v-model="string" @change="onChange" v-bind:value="station">{{station}}</md-checkbox>
-        </li>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-        v-if="string != null && renderComponent"
-      >
-        <chart-card
-          :chart-data="TemperatureChart.data"
-          :chart-options="TemperatureChart.options"
-          :chart-type="'Line'"
-          data-background-color="blue"
-        >
-          <template slot="content">
-            <h4 class="title">Temperature chart</h4>
-
-            <label for="start">Start date:</label>
-            <input type="date" id="start" name="trip-start"
-                  v-model="TemperatureChart.dates.dateDebut"
-                  value="2022-03-04"
-                  min="2022-01-01" max="2022-12-31">
-
-            <label for="start">Stop date:</label>
-            <input type="date" id="stop" name="trip-stop"
-                  v-model="TemperatureChart.dates.datefin"
-                  value="2022-03-05"
-                  min="2022-01-01" max="2022-12-31">
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              just updated
-            </div>
-          </template>
-        </chart-card>
-      </div>
+      
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-        v-if="string != null && renderComponent"
+        v-for="(station,index) in ChoosenStations" :key="index"
       >
         <md-card>
           <md-card-header data-background-color="red">
-            <h4 class="title">Last Datas</h4>
+            <h4 class="title">Last Datas of {{station}}</h4>
             <p class="category">lasts datas from the station</p>
           </md-card-header>
           <md-card-content>
-            <ordered-table table-header-color="red" v-bind:sonde=this.string></ordered-table>
+            <ordered-table table-header-color="red" v-bind:sonde=station></ordered-table>
           </md-card-content>
         </md-card>
       </div>
@@ -63,43 +22,17 @@
 
 <script>
 import {
-  ChartCard,
   OrderedTable,
 } from "@/components";
 
 export default {
   components: {
-    ChartCard,
     OrderedTable,
   },
   data() {
     return {
       string : null,
       renderComponent : true,
-      tableDatas : [],
-      TemperatureChart: {
-        dates: {
-          dateDebut: new Date(),
-          datefin: new Date(),
-        },
-        data: {
-          labels: [],
-          series: [],
-        },
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0,
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          },
-        },
-      },
     };
   },
   computed : {
@@ -119,24 +52,7 @@ export default {
           this.renderComponent = true;
         })
     },
-
-    changeTempChart() {
-      console.log("http://"+this.string+":8080/data/temperature/"+this.TemperatureChart.dates.dateDebut+","+this.TemperatureChart.dates.datefin)
-      fetch("http://"+this.string+":8080/data/temperature/"+this.TemperatureChart.dates.dateDebut+","+this.TemperatureChart.dates.datefin)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
-      })
-    }
   }
 };
 </script>
-<style scoped>
 
-  .ChooseStation{
-    display : flex;
-    flex-direction: row;
-    justify-content: space-around;
-  }
-
-</style>
